71873ecdde660be3c134c10e00c912a91236aa4d
Erick Silva <erick.silva@firstdecision.com.br>
2021-05-07 10:47:21 -0300

    fix: database password

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
+import dotenv from "dotenv";
 import app from './app';
 import 'reflect-metadata';
 import './database';
 
-app.listen(3000, () => {
-  console.log('🏃 Running Server');
+dotenv.config();
+
+const port = process.env.PORT || 3055;
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
 });
